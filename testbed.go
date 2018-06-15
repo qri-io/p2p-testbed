c22b2ce082e7cb95e9fab3b7760d42b0087b6a53
Kasey <klhuizinga@gmail.com>
2018-06-15 17:45:01 -0400

    fix(testbed.go): fix logging import

--- testbed.go
+++ testbed.go
@@ -8,8 +8,8 @@ import (
 	"net/url"
 	"os"
 
-	logging "github.com/ipfs/go-log"
-	// logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	// logging "github.com/ipfs/go-log"
+	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
 
 	opentracing "github.com/opentracing/opentracing-go"
 	"sourcegraph.com/sourcegraph/appdash"
